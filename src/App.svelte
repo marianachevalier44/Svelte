4e8aecea8e0e26fe427a96309865ba89995f992c
Mariana <mariana.chevalier.mc@gmail.com>
2021-05-11 15:48:40 +0200

    version 2

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,41 @@
+<script>
+    import Login from "./Login.svelte";
 
+    let isLogin = false;
 
-                <script>
-                    import Login from './Login.svelte';
+    function loginBtn() {
+        isLogin = true;
+    }
 
-                    let isLogin = false;
+    function close() {
+        isLogin = false;
+    }
 
-                    function loginBtn() {
-                        isLogin = true;
-                    }
+    function getAnimals() {
+        fetch('http://localhost:8080/Animal')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            });
+    }
+</script>
 
-                    function close() {
-                        isLogin = false;
-                    }
-                </script>
+<main>
+    <button
+        class="px-5 py-3 font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+        on:click={loginBtn}
+        >Ajouter un animal
+    </button>
 
-                <main>
-                    <button class="px-5 py-3 font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
-                            on:click={loginBtn}>Ajouter un animal
-                    </button>
-                    {#if isLogin}
-                        <Login on:closeIt={close}></Login>
-                    {/if}
-                </main>
+    <button
+        class="px-5 py-3 font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
+        on:click={getAnimals}
+        >get animal
+    </button>
 
+    {#if isLogin}
+        <Login on:closeIt={close} />
+    {/if}
+</main>
--- src/Login.svelte
+++ src/Login.svelte
@@ -1,6 +1,6 @@
-<script>
-    import {createEventDispatcher} from 'svelte';
-    import {fly, fade} from 'svelte/transition';
+<script xmlns="http://www.w3.org/1999/html">
+    import { createEventDispatcher } from 'svelte';
+    import { fly, fade} from 'svelte/transition';
 
     const dispatch = createEventDispatcher();
 
@@ -20,55 +20,101 @@
         background-color: rgba(0, 0, 0, 0.5);
         z-index: 2;
     }
-
     .login-box {
         position: fixed;
-        margin: 20vh auto;
-        height: 200px;
-        width: 400px;
-        /*color: white;*/
-        padding: 20px;
-        background-color: rgb(218, 218, 255);
-        border: solid 5px white;
+        margin: 0;
+        padding: 5px;
+        height: 300px;
+        width: 800px;
+        /*
+        color: white;
+
+        background-color: rgb(42,42,187);
+        border: solid 5px white;*/
         z-index: 5;
+    }
+    button{
+        background: #1f2937;
+    }
+    input[type="text"]{
+        display: inline-block;
+        margin: 5px;
     }
 
 </style>
 
-<div class="background" on:click={close} transition:fade/>
+<div class="background" transition:fade on:click={close}/>
 <div class="login-box" transition:fly={{y: -500}}>
-    <div class=" container px-6 mx-auto grid">
-        <div class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300">
-            <div
-                    class="px-4 py-3 mb-8 bg-white rounded-lg shadow-md dark:bg-gray-800"
-            >
-                <label class="block text-sm">
-                    <span class="text-gray-700 mt-1.5 dark:text-gray-400">Prénom</span>
-
-                    <input class="block w-full mt-1.5 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-                    />
-                </label>
-
-
-            </div>
-            <button class="px-3 py-1 text-sm font-medium leading-5 text-white
-            transition-colors duration-150 bg-purple-600 border border-transparent
-            rounded-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none
-            focus:shadow-outline-purple" on:click={close}>
-                Enregistrer
-
-            </button>
-
-            <button class="px-3 py-1 text-sm font-medium leading-5 text-white
-            transition-colors duration-150 bg-purple-600 border border-transparent
-            rounded-md active:bg-purple-600 hover:bg-purple-700 focus:outline-none
-            focus:shadow-outline-purple" on:click={close}>Close
-            </button>
+    <!--form-- action="">
+        <label id="username" for="username">Username:
+            <input type="text"><br>
+        </label>
+        <label id="pasword" for="pasword">Password:
+            <input type="password"><br>
+        </label>
+        <button on:click={close}> Login</button>
+        <button on:click={close}> Close</button>
+    </form-->
+    <div class="hidden sm:block" aria-hidden="true">
+        <div class="py-5">
+            <div class="border-t border-gray-200"></div>
         </div>
     </div>
-    <!--form action="">
-        <label for="username" id="username">Username:<input type="text"></label>
-        <label for="password" id="password">Password:<input type="password"></label>
 
-    </form-->
+    <div class="mt-0 sm:mt-0">
+        <div class="md:grid md:grid-cols-3 md:gap-6">
+
+            <div class="px-0 md:col-span-1">
+                <div class="px-0 sm:px-0">
+                    <h3 class="text-lg font-medium leading-6 text-white">Informations</h3>
+                </div>
+            </div>
+            <div class="mt-5 md:mt-0 md:col-span-2">
+                <form action="#" method="POST">
+                    <div class="shadow overflow-hidden sm:rounded-md">
+                        <div class="px-4 py-5 bg-indigo-100 sm:p-6">
+                            <div class="grid grid-cols-6 gap-6">
+                                <div class="col-span-6 sm:col-span-4">
+                                    <label for="name" class="block text-sm font-medium text-gray-700">Prénom</label>
+                                    <input type="text" name="name" id="name" autocomplete="name" placeholder="Prénom" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                </div>
+
+                                <div class="col-span-4 sm:col-span-4">
+                                    <label for="genre" class="block text-sm font-medium text-gray-700">Genre</label>
+                                    <input type="text" name="genre" id="genre" autocomplete="genre" placeholder="Femelle/Mâle" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                </div>
+
+                                <div class="col-span-6 sm:col-span-3">
+                                    <label for="espece" class="block text-sm font-medium text-gray-700">Espèce</label>
+                                    <input type="text" name="espece" id="espece" autocomplete="espece" placeholder="Chat,Chien,etc." class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                </div>
+
+                                <div class="col-span-6 sm:col-span-2">
+                                    <label for="date_naissance" class="block text-sm font-medium text-gray-700">Date de naissance</label>
+                                    <input type="date" name="date_naissance" id="date_naissance" autocomplete="Date de naissance" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                </div>
+
+                                <div class="col-span-6 sm:col-span-2">
+                                    <label for="poid" class="block text-sm font-medium text-gray-700">Poid</label>
+                                    <input type="text" name="poid" id="poid" autocomplete="KG" placeholder="Kg" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                </div>
+
+                                <div class="col-span-6 sm:col-span-3">
+                                    <label for="race" class="block text-sm font-medium text-gray-700">Race</label>
+                                    <input type="text" name="race" id="race" autocomplete="family-name" placeholder="Race" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                            <button on:click={close} type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                Enregistrer
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
+
